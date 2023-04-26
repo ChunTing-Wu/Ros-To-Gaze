d1cb68b9206309a9e26413f89ee229937df64e76
BarryWu_0222 <barrywu0222@gmail.com>
2023-04-26 15:00:02 +0800

    Update CSS

--- ROS_UDP/WebSocketTest.py
+++ ROS_UDP/WebSocketTest.py
@@ -1,20 +1,20 @@
 import asyncio
 import websockets
 
-# 儲存所有 WebSocket 連接的客戶端
 clients = set()
 
 async def echo(websocket, path):
-    # 將客戶端加入 clients 集合
     clients.add(websocket)
+
+    websocket.max_size = 1024
+
     try:
         async for message in websocket:
-            print(message,'received from client')
-            # 將收到的資料發送給所有客戶端
             for client in clients:
                 await client.send(message)
+    except websockets.exceptions.ConnectionClosedError as e:
+        print(f"Connection to client {websocket.remote_address} closed: {e}")
     finally:
-        # 將客戶端從 clients 集合中移除
         clients.remove(websocket)
 
 asyncio.get_event_loop().run_until_complete(
